85801a8f591f8d3e201569b4c895fa7bfda7ba08
Raluca <raluca.paraschiv@student.upt.ro>
2026-05-12 17:41:04 +0300

    Initial commit

---
+++ src/main/java/io/github/ralucap11/medicalimagingtystem/MedicalImagingSystemApplication.java
@@ -0,0 +1,13 @@
+package io.github.ralucap11.medicalimagingtystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicalImagingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedicalImagingSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/ralucap11/medicalimagingtystem/MedicalImagingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.ralucap11.medicalimagingtystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedicalImagingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
